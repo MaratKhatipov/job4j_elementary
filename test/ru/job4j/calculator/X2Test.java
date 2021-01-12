a9076076b0896803a84c811465db6b907f234122
MaratKhatipov <mmmarat86@gmail.com>
2021-01-12 14:01:09 +0500

    9.2. Checkstyle [#442699]

--- test/ru/job4j/calculator/X2Test.java
+++ test/ru/job4j/calculator/X2Test.java
@@ -18,7 +18,6 @@ public class X2Test {
 	}
 
 	@Test
-
 	public void whenA0B1C1X1Then2() {
 		int a = 0;
 		int b = 1;
@@ -30,7 +29,6 @@ public class X2Test {
 	}
 
 	@Test
-
 	public void whenA1B1C0X1Then2() {
 		int a = 1;
 		int b = 1;
@@ -42,7 +40,6 @@ public class X2Test {
 	}
 
 	@Test
-
 	public void whenA1B1C1X1Then1() {
 		int a = 1;
 		int b = 1;
