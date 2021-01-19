80e1ec3cceaee6dbf789012a32e12ccfb03a22d8
MaratKhatipov <mmmarat86@gmail.com>
2021-01-19 09:21:33 +0500

    6.4.2. Слово заканчивается на ... [#442656]

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -4,15 +4,14 @@ public class EndsWith {
 	public static boolean endsWith(char[] word, char[] post) {
 		boolean result = true;
 		for (int i = (word.length - post.length); i < word.length; i++) {
-			for (int j = 0; j < post.length; j++) {
 
-				if (post[j] == word[i]) {
+				if (post[i - (word.length - post.length)] == word[i]) {
 					result = true;
 
 				} else {
 					result = false;
 				}
-			}
+
 		}
 		return result;
 	}
--- test/ru/job4j/array/EndsWithTest.java
+++ test/ru/job4j/array/EndsWithTest.java
@@ -22,4 +22,20 @@ public class EndsWithTest {
 		boolean result = EndsWith.endsWith(word, post);
 		assertThat(result, is(false));
 	}
+
+	@Test
+	public void whenNotEndWithPrefixThenFalse1() {
+		char[] word = {'D', 'o', 'g', 'o', 'v', 'o', 'r'};
+		char[] post = {'o', 'v', 'o', 'l'};
+		boolean result = EndsWith.endsWith(word, post);
+		assertThat(result, is(false));
+	}
+
+	@Test
+	public void whenNotEndWithPrefixThenTrue1() {
+		char[] word = {'D', 'o', 'g', 'o', 'v', 'o', 'r', 'l', 'i', 'z', 'i', 'n', 'g', 'a'};
+		char[] post = {'l', 'i', 'z', 'i', 'n', 'g', 'a'};
+		boolean result = EndsWith.endsWith(word, post);
+		assertThat(result, is(true));
+	}
 }
